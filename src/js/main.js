84d7ead34e4d55b53709a970079e145a40d37b8d
Stephen Roberts <stephen.roberts@dish.com>
2017-08-15 20:03:49 -0700

    Implement pure css menu

--- src/js/main.js
+++ src/js/main.js
@@ -5,72 +5,16 @@
 
   // DOM elements
   const navMenu = document.getElementById('nav-menu')
-  const navToggleBtn = document.getElementById('nav-toggle-btn')
+  const navToggleCheckbox = document.getElementById('nav-toggle-checkbox')
   const contactForm = document.getElementById('contact-form')
   const contactAlert = document.getElementById('contact-alert')
   const contactSpinner = document.getElementById('contact-spinner')
 
-  const closeMenu = () => {
-
-    // Set initial height to natural height
-    navMenu.style.height = `${navMenu.clientHeight}px`
-
-    navMenu.dataset.expanded = false
-
-    // Set transition value in a non-blocking thread
-    setTimeout(() => {
-      navMenu.classList.add('nav-transition')
-      navMenu.style.height = 0
-    }, 100)
-  }
-
-  const openMenu = () => {
-
-    // Calculate natural height
-    navMenu.style.height = 'auto'
-    const height = navMenu.clientHeight
-
-    // Set initial height
-    navMenu.style.height = 0
-
-    navMenu.dataset.expanded = true
-
-    // Set transition value in a non-blocking thread
-    setTimeout(() => {
-      navMenu.classList.add('nav-transition')
-      navMenu.style.height = `${height}px`
-    }, 100)
-  }
-
-  // Toggle menu
-  navToggleBtn.addEventListener('click', (e) => {
-    if (navMenu.dataset.expanded == "true") {
-      closeMenu()
-    } else {
-      openMenu()
-    }
-  })
-
-  // When nav view finishes animating, set default height
-  navMenu.addEventListener('transitionend', (e) => {
-
-    // Remove transition and set back to default height
-    navMenu.classList.remove('nav-transition')
-    if (e.target.dataset.expanded == "true")
-      e.target.style.height = 'auto'
-    else
-      e.target.style.height = null
-  })
-
   // Close menu when item is selected
   navMenu.addEventListener('click', (e) => {
-
-    // Ignore this event if the menu isn't "expanded"
-    if (navMenu.dataset.expanded != "true") return
-    
     // If a nav link item was clicked, close menu
     if (e.target.classList.contains('link')) {
-      closeMenu()
+      navToggleCheckbox.checked = false;
     }
   })
 
